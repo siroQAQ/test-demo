3c7e0202f70d1cc2cc0875d76169935dc3b5a258
siroQAQ <853787248@qq.com>
2025-05-17 16:42:03 +0800

    Update test_bing.py

--- test_bing.py
+++ test_bing.py
@@ -1,12 +1,11 @@
-from time import sleep
+import allure
 import requests
 
+@allure.title("访问必应首页")
 def test_访问必应():
-    # Step 1: 发送 GET 请求
-    response = requests.get("https://www.bing.com")
-    
-    # Step 2: 提取状态码
-    code = response.status_code
+    with allure.step("发送 GET 请求"):
+        response = requests.get("https://www.bing.com")
 
-    # Step 3: 验证状态码等于 200
-    assert code == 200, f"断言失败，实际状态码为: {code}"
+    with allure.step("校验响应状态码"):
+        code = response.status_code
+        assert code == 200, f"断言失败，实际状态码为: {code}"
